c648c387aa4456dfdb84b27b0bce8729ddbda3a7
agandik <dikiagustiann@gmail.com>
2022-02-04 12:06:43 +0800

    memperbaiki tampilan button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,18 +8,22 @@
                 <h4 class="fw-bold" data-aos="fade-left">Hello, Welcome to <span>BacaAja</span></h4>
                 <div class="row download-cv mt-5 mb-5">
                     <div class="col-lg-5">
-                        <a href="javascript:delay('/dashboard')">
-                            <lottie-player id="playlottie" src="https://assets8.lottiefiles.com/packages/lf20_wotq4bi0.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"></lottie-player>
-                        </a>
-                    </div>
-                    <div class="col-lg-5">
-                        <a href="javascript:delay('/posts')">
-                            <lottie-player id="playlottie1" src="https://assets2.lottiefiles.com/packages/lf20_u5n5hcrd.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"></lottie-player>
-                        </a>
+                        <div class="row">
+                            <div class="col col-lg-3 me-auto">
+                                <a href="javascript:delay('/dashboard')">
+                                    <lottie-player id="playlottie" src="https://assets5.lottiefiles.com/packages/lf20_b6ocdgzo.json"  background="transparent"  speed="1"  style="width: 150px; height: 150px;"></lottie-player>
+                                </a>
+                            </div>
+                            <div class="col col-lg-3 ms-auto">
+                                <a href="javascript:delay('/posts')">
+                                    <lottie-player id="playlottie1" src="https://assets6.lottiefiles.com/packages/lf20_pmn7hvzy.json"  background="transparent"  speed="1"  style="width: 150px; height: 150px;"></lottie-player>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-5 ms-auto">
+            <div class="col col-lg-5 ms-auto">
                 <lottie-player src="https://assets1.lottiefiles.com/packages/lf20_pov5id7i.json"  background="transparent"  speed="1"  style="width: auto; height: 90vh;"  loop autoplay></lottie-player>
             </div>
         </div>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -10,7 +10,7 @@
             <a class="nav-link {{ Request::is('/')?'active':'' }}" href="/">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link {{ Request::is('posts')?'active':'' }}" href="/posts">Blog</a>
+            <a class="nav-link {{ Request::is('posts','post*')?'active':'' }}" href="/posts">Blog</a>
           </li>
           <li class="nav-item">
             <a class="nav-link {{ Request::is('categories')?'active':'' }}" href="/categories">Categories</a>
